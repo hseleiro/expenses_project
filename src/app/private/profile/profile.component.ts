3024423b29f5336616cc7007c29f564c4bae70a8
hseleiro <hseleiro@gmail.com>
2023-03-05 20:29:54 +0000

    EXP-001 - Add NGRX angular router state

--- src/app/private/profile/profile.component.ts
+++ src/app/private/profile/profile.component.ts
@@ -1,23 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {ProfilePageActions} from "./actions";
 import {Store} from "@ngrx/store";
+import {SharedService} from "../../shared/services/shared.service";
+import {RouterStateUrl} from "../../shared/router-state/custom-serializer";
+import {RouterReducerState} from "@ngrx/router-store";
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, OnDestroy {
+  private routerStore: RouterReducerState<RouterStateUrl> | undefined;
 
-  constructor(private router: Router, private store: Store,) { }
+  constructor(private router: Router, private store: Store, private sharedService: SharedService) { }
 
   ngOnInit(): void {
+    this.routerStore = this.sharedService.getRouterState;
+    console.log('this.routerStore',  this.routerStore)
+
+    // Page actions
     this.store.dispatch(ProfilePageActions.enter())
+  }
+
+  ngOnDestroy() {
+
   }
 
   navigateTo(routeName: string):void {
     this.router.navigate([`/public/${routeName}`]);
-
   }
 }
--- src/app/shared/state/index.ts
+++ src/app/shared/state/index.ts
@@ -1,18 +1,26 @@
 import * as fromWelcome from "./welcome.reducer";
 import * as fromTodo from "./todo.reducer";
 import * as fromTProfile from "./profile.reducer";
-import {ActionReducerMap, MetaReducer} from "@ngrx/store";
+import * as fromRouter from '@ngrx/router-store';
+import {ActionReducerMap, createFeatureSelector, createSelector, MetaReducer} from "@ngrx/store";
+import {routerReducer } from "@ngrx/router-store";
+import {RouterStateUrl} from "../router-state/custom-serializer";
 
 export interface State {
   welcome: fromWelcome.State,
   todo: fromTodo.State
   profile: fromTProfile.State
+  router: fromRouter.RouterReducerState<any>;
 }
 
 export const reducers: ActionReducerMap<State> = {
   welcome: fromWelcome.reducer,
   todo: fromTodo.reducer,
-  profile: fromTProfile.reducer
+  profile: fromTProfile.reducer,
+  router: routerReducer
 }
 
+export const getRouterState = createFeatureSelector<
+  fromRouter.RouterReducerState<RouterStateUrl>
+>('router');
 export const metaReducers: MetaReducer<State>[] = [];
